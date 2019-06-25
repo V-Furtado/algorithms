dad4e49cbd889ffe6c6f3091cdd8bd1310188ffc
vanderley Furtado <vanderley@178.50.20.172.s.wireless.uri.edu>
2019-06-25 14:47:07 -0400

    sorts

--- source/string.cpp
+++ source/string.cpp
@@ -1,2 +1,59 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "string.hpp"
+
+char at(int) const;
+
+bool empty(){
+    return true;
+}
+
+unsigned int size() const;
+
+unsigned int capacity() const;
+
+void reserve(unsigned int);
+
+void insert(char, int);
+
+void erase(char){
+
+}
+
+void remove(int){
+
+}
+
+void append(char){
+
+}
+
+void prepend(char){
+
+}
+
+bool compare(char*) const;
+bool compare(String&) const;
+
+
+void concatenate(char*){
+
+}
+void concatenate(String&){
+
+}
+
+unsigned int find(char*, int start = 0) const;
+unsigned int find(char, int start = 0) const;
+unsigned int find(String&, int start = 0) const;
+
+void reverse(){
+
+}
+
+void shift(int){
+
+}
+
+int toInt() const;
+
+String substr(int, int) const;
--- source/string.hpp
+++ source/string.hpp
@@ -27,84 +27,84 @@ class String {
    * Returns whether or not the string is empty.
    * O(?)
    */
-  bool empty() const;
+  bool String::empty() const;
 
   /** size()
    * Returns the number of characters in the string.
    */
-  unsigned int size() const;
+  unsigned int String::size() const;
 
   /** capacity()
    * Returns the number of characters that the string can hold.
    * O(?)
    */
-  unsigned int capacity() const;
+  unsigned int String::capacity() const;
 
   /** reserve(int extra)
    * Reserves extra amount of characters, extending the capacity of the string.
    * O(?)
    */
-  void reserve(unsigned int);
+  void String::reserve(unsigned int);
 
   /** insert(char c, int index)
    * Inserts the given character `c` into the string at the given index.
    * If the index is out of bounds the character should appended / prepended.
    * O(?)
    */
-  void insert(char, int);
+  void String::insert(char, int);
 
   /** erase(char)
    * Erases all copies of the given character from the string.
    * O(?)
    */
-  void erase(char);
+  void String::erase(char);
 
   /** remove(int index)
    * Removes the character at the given index.
    * O(?)
    */
-  void remove(int);
+  void String::remove(int);
 
   /** append(char)
    * Appends the given character to the string.
    * O(?)
    */
-  void append(char);
+  void String::append(char);
 
   /** prepend(char)
    * Prepends the given character to the string.
    * O(?)
    */
-  void prepend(char);
+  void String::prepend(char);
 
   /** compare(char* or String)
    * Returns whether or not the string is equal with the one given.
    * O(?)
    */
-  bool compare(char*) const;
-  bool compare(String&) const;
+  bool String::compare(char*) const;
+  bool String::compare(String&) const;
 
   /** concate(char* or String)
    * Concatenates the string with the given character array, or string.
    * O(?)
    */
-  void concatenate(char*);
-  void concatenate(String&);
+  void String::concatenate(char*);
+  void String::concatenate(String&);
 
   /** find(char* or char or String)
    * Returns the index of the first occurrence of the character array, char, or
    * string given. Should return length if not found.
    * O(?)
    */
-  unsigned int find(char*, int start = 0) const;
-  unsigned int find(char, int start = 0) const;
-  unsigned int find(String&, int start = 0) const;
+  unsigned int String::find(char*, int start = 0) const;
+  unsigned int String::find(char, int start = 0) const;
+  unsigned int String::find(String&, int start = 0) const;
 
   /** reverse()
    * Reverses the string in-place.
    * O(?)
    */
-  void reverse();
+  void String::reverse();
 
   /** shift(int)
    * Shifts all characters ASCII values in the array upwards or downwards by the
@@ -112,20 +112,20 @@ class String {
    * null, or exceeding the ASCII range.
    * O(?)
    */
-  void shift(int);
+  void String::shift(int);
 
   /** toInt()
    * Returns the string as an integer, if it can be represented as one.
    * Otherwise throws an exception
    * O(?)
    */
-  int toInt() const;
+  int String::toInt() const;
 
   /** substr(start, end)
    * Returns the string contained between [start, end).
    * O(?)
    */
-  String substr(int, int) const;
+  
 };
 
 TEST_CASE("Append") {
